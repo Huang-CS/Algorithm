81c1545986ddc8cc1fa74d1cf53a02d906c04926
Huang-CS <Huang-CS@users.noreply.github.com>
2018-04-30 15:20:56 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,37 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Test
 {
     public static void main (String[] args)
+    {
+            sortArray();
+    }
+    
+    private static void sortArray()
+    {
+        int a[] = randomIntArray(20,100);
+        System.out.println(Arrays.toString(a));
+            
+    }
+    
+    
+    public static int[] randomIntArray(int length, int n)
+   {  
+      Random generator = new Random();
+        int[] a = new int[length];      
+      for (int i = 0; i < a.length; i++)
+      {
+         a[i] = generator.nextInt(n);//between 0 and n-1
+      }
+      
+      return a;
+   }
+    
+    
+    
+    private static void permutation()
     {
         Permutation input = new Permutation("abcd");
         ArrayList<String> result = input.permutate();
@@ -10,7 +39,8 @@ public class Test
         for(String s:result)
         {
             System.out.println(s);
-        }
-        
+        }   
     }
+    
+    
 }
